afac6a51dcc9e4a79c99bcca35cdbcd8eaafdae3
ANA FLÁVIA BUENO GOMES <06006399@acad.unifeso.lan>
2024-06-10 20:17:29 -0300

    perf: optimizing the code

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', async function() {
     const nameElement = document.querySelector("#name");
     const usernameElement = document.querySelector("#username");
     const avatarElement = document.querySelector("#avatar");
@@ -8,16 +8,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
     fetch('https://api.github.com/users/lyannabueno')
 
-    .then(function(resposta) {
-        return resposta.json()
-    })
+    try {
+        const response = await fetch('https://api.github.com/users/lyannabueno');
 
-    .then(function(json) {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const json = await response.json();
+
         nameElement.innerHTML = json.name
         usernameElement.innerHTML = json.login
         avatarElement.innerHTML = json.avatar_url
         followingElement.innerHTML = json.following
         followersElement.innerHTML = json.followers
         reposElement.innerHTML = json.public_repos
-    })
+    } catch(erro) {
+        alert("Ocorreu um erro ao buscar o endereço, tente novamente mais tarde!")
+    } finally {
+        console.log('Operação fetch finalizada.');
+    }
 })
